ceeb8db8b0083fbb5b7d5b74d7bd2f98d4d66eaa
Seong ho Hong <seonghohong@Seongui-MacBook-Air.local>
2016-12-28 23:18:48 +0900

    save

---
+++ project1/src/main/java/MainForm.java
@@ -0,0 +1,15 @@
+/**
+ * Created by seonghohong on 2016. 12. 28..
+ */
+public class MainForm {
+
+    public static void main(String[] args){
+
+        askInputNumber.askNumber();
+
+        findThreeMultiple.getMultiple();
+        findFourMultiple.getMultiple();
+
+        deleteSameMultiple.deleteSameNumber();
+    }
+}
